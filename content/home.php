9cf28f25d7610b402a240b17b1c1579b31e6902a
Hannes Eilers <mail@hanneseilers.de>
2020-03-18 08:22:12 +0100

    Added home content

--- content/home.php
+++ content/home.php
@@ -1,4 +1,37 @@
-<p>
-	phpBasicCMS<br />
-	See information on <a href="https://github.com/hanneseilers/phpBasicCMS">https://github.com/hanneseilers/phpBasicCMS</a>
-</p>
+<div class="d-flex justify-content-center p-3">
+
+	<div class="container">
+	
+		<div class="row">
+			<div class="col-md-12 bg-primary text-light p-2">
+				
+				<h1>Nachbarshilfe:</h1>
+				
+			</div>
+		</div>
+		
+		<hr />
+		
+		<div class="row">
+			
+			<div class="col-md-10">
+				<p>
+					<a href=""><button class="btn btn-lg btn-success"><i class="fas fa-plus"></i> Biete Hilfe</button></a>
+					<a href=""><button class="btn btn-lg btn-danger"><i class="fa fa-plus fa-fw"></i> Suche Hilfe</button></a>
+				</p>
+			</div>				
+			
+			<div class="col-md-2">
+				<p>
+					<a href=""><button class="btn btn-lg btn-info"><i class="fa fa-plus fa-fw"></i> Login</button></a>
+				</p>
+				<p>
+					<a href=""><button class="btn btn-lg btn-info"><i class="fa fa-plus fa-fw"></i> Registrieren</button></a>
+				</p>
+			</div>
+				
+		</div>
+	
+	</div>
+
+</div>
