20c623624e8c67fc49caaaac427988ef51e7cd32
Hannes Eilers <mail@hanneseilers.de>
2020-03-18 14:45:58 +0100

    aded user id hidden data

--- content/user-info.php
+++ content/user-info.php
@@ -10,14 +10,19 @@
 	<?php $usr = $_SESSION['user'] ?>
 	
 	<small>*Plichtangaben</small>
+	
+	<span id="userid" value="<?php print $usr['id'] ?>" hidden></span>
+	
 	<div class="form-group">						
 		<label for="name">Name*</label>
-		<input type="text" class="form-control" id="name" aria-describedby="Ihr Name" placeholder="Ihr Name" required>
+		<input type="text" class="form-control" id="name" aria-describedby="Ihr Name" placeholder="Ihr Name"
+			value="<?php print( base64_decode($usr['name']) );?>" required>
 	</div>
 	
 	<div class="form-group">						
 		<label for="plz">PLZ*</label>
-		<input type="text" class="form-control" id="plz" aria-describedby="Postleitzahl" placeholder="Postleitzahl" required>
+		<input type="text" class="form-control" id="plz" aria-describedby="Postleitzahl" placeholder="Postleitzahl"
+			value="<?php print( base64_decode($usr['plz']) );?>" required>
 	</div>
 	
 	<div class="form-group">						
@@ -30,20 +35,23 @@
 	<b>entweder*:</b>
 	<div class="form-group">						
 		<label for="phone">Telefon</label>
-		<input type="tel" class="form-control" id="phone" aria-describedby="Ihre Telefonnummer" placeholder="Ihre Telefonnummer">
+		<input type="tel" class="form-control" id="phone" aria-describedby="Ihre Telefonnummer" placeholder="Ihre Telefonnummer"
+			value="<?php print( base64_decode($usr['phone']) );?>">
 	</div>	
 	
 	<b>oder*</b>				
 	<div class="form-group">					
 		<label for="mail">E-Mail Adresse</label>
-		<input type="email" class="form-control" id="mail" aria-describedby="E-Mail Adresse" placeholder="E-Mail Adresse">
+		<input type="email" class="form-control" id="mail" aria-describedby="E-Mail Adresse" placeholder="E-Mail Adresse"
+			value="<?php print( base64_decode($usr['mail']) );?>">
 	</div>
 	
 	<hr />
 	
 	<div class="form-group">						
 		<label for="adress">Adresse</label>
-		<textarea type="text" class="form-control" id="adress" aria-describedby="Ihre Adresse" placeholder="Ihre Adresse" rows=4></textarea>
+		<textarea type="text" class="form-control" id="adress" aria-describedby="Ihre Adresse" placeholder="Ihre Adresse" rows=4
+			value="<?php print( base64_decode($usr['adress']) );?>"></textarea>
 	</div>	
 		
 </p>	
@@ -54,8 +62,10 @@
 	<script>
 		$(document).ready(function(){
 			$(".form-group > input").keypress(function(event){
-				if( event.type == "keypress" && event.which == 13 )
+				if( event.type == "keypress" && event.which == 13 ){
 					// TODO
+				
+				}
 			});
 		});
 	</script>					
