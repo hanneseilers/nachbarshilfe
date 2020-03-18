7314630a14153a2121f164032c140170ed5ee485
Hannes Eilers <mail@hanneseilers.de>
2020-03-18 13:53:04 +0100

    added showing userdata if logged in

--- content/home.php
+++ content/home.php
@@ -24,57 +24,14 @@
 			
 			<div class="col-md-3">
 			
-				<h3>Login</h3>
-				<div class="alert alert-danger" id = "err_loginfailed" style="display: none;"><small>Login fehlgeschlagen</small></div>
-				<hr />
-			
-				<!-- Load auth plugin -->
-				<?php getJs('auth'); ?>
-				<span id="authurl" value="<?php global $php; print $php."auth.php" ?>" hidden></span>
-				
-				<p>				
-					<b>entweder:</b>
-					<div class="form-group">						
-						<label for="phone">Telefon</label>
-						<input type="tel" class="form-control" id="phone" aria-describedby="Ihre Telefonnummer" placeholder="Ihre Telefonnummer">
-					</div>	
-					
-					<b>oder</b>				
-					<div class="form-group">					
-						<label for="mail">E-Mail Adresse</label>
-						<input type="email" class="form-control" id="mail" aria-describedby="E-Mail Adresse" placeholder="E-Mail Adresse">
-					</div>
-					
-					<hr />
-					
-					<div class="form-group">						
-						<label for="pw">Passwort</label>
-						<input type="password" class="form-control" id="pw" aria-describedby="Passwort" placeholder="Passwort" required>
-					</div>
-					
-					<p>
-					
-					<button class="btn btn-info" onClick="login();"><i class="fas fa-sign-in-alt"></i> Login</button>
-					
-					<script>
-						$(document).ready(function(){
-							$(".form-group > input").keypress(function(event){
-								if( event.type == "keypress" && event.which == 13 )
-									login();
-							});
-						});
-					</script>					
-				</p>		
-				
-				<hr />
-				
-				<p>
-					Noch keinen Account? Jetzt anmelden und helfen!
-				</p>
-				
-				<p>
-					<a href="?register"><button class="btn btn-info"><i class="fa fa-plus fa-fw"></i> Anmelden</button></a>
-				</p>
+				<?php
+					if( isset($_SESSION['user']) ){
+						include( $content."user-info.php" );
+					} else{
+						global $content;
+						include( $content."login-form.php" );
+					}
+				?>
 				
 			</div>
 				
