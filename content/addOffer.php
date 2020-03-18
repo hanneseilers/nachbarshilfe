7a8e11c4569b89bcdffbc3e68580b40664727835
Hannes Eilers <mail@hanneseilers.de>
2020-03-18 19:25:33 +0100

    Completed form and created page to process.

---
+++ content/addOffer.php
@@ -0,0 +1,15 @@
+<?php global $cfg; ?>
+<div class="d-flex justify-content-center p-3">
+
+	<div class="container">
+		
+		<?php
+		
+		if( isset($_GET['amount'] ) ){
+		}
+		
+		?>
+		
+	</div>
+
+</div>
--- content/offer.php
+++ content/offer.php
@@ -2,20 +2,51 @@
 <div class="d-flex justify-content-center p-3">
 
 	<div class="container">
+	<form action="<?php url('addOffer'); ?>" method="get">
 		
 		<div class="row">
-			<div class="col-md-8 bg-success">
+			<div class="col-md-8">
 			
-				<div class="row">
-					<div class="col-md-12 bg-info text-light p-2">
-						<h3>Hilfe anbieten</h3>
+				<div class="">
+					<div class="col-md-12 bg-success text-light p-2">
+						<h3><i class="fas fa-hands-helping fa-fw"></i> Hilfe anbieten</h3>
 					</div>
+					
+					<hr />
+					
+					<div class="form-group">						
+						<label for="amount">Menge?</label>
+						<select multiple class="form-control" id="amount" aria-describedby="Menge an Hilfe">
+							<option value=10 selected>Wenig</option>
+							<option value=30>Mittel</option>
+							<option value=90>Viel</option>
+						</select>
+						<small>
+							Wenig: ca. 1-2 mal pro Woche insgesamt 5-10 Teile einkaufen. Keine Extra-Touren.<br/>
+							Mittel: ca. 2-3 mal pro Woche insgesamt 10-30 Teile einkaufen. Extra-Touren nur wenn unbedingt n&ouml;tig.<br />
+							Viel: min. 3 mal pro Woche bis zu 90 Teile einkaufen. Es geht mehr und Extra-Touren.
+						</small>
+					</div>
+					
+					<div class="form-group">						
+					<label for="adress">Beschreibung</label>
+					<textarea type="text" class="form-control" id="adress" aria-describedby="Ihre Text" placeholder="Beschreibung (optional)" rows=4></textarea>
+				</div>	
+					
 				</div>
 				
 			</div>
 			
 			<div class="col-md-4">
-				#ADD#
+				<p>
+					<button class="btn btn-lg btn-success" type="submit"><i class="fas fa-plus"></i> Hilfe anbieten</button>
+				</p>
+				<p>
+					<i class="fas fa-exclamation"></i> Bitte biete nur Hilfe an, die du auch wirklich leisten kannst. Übernehm dich nicht! Hilf lieber nur ein wenig, dafü&uuml;r aber verl&aum,l;sslich.
+				</p>
+				<p>
+					<small>Falls du noch nicht angemeldet bist, f&uuml;ll einfach das Anmeldformular mit aus.</small>
+				</p>
 			</div>
 			
 		</div>
@@ -88,21 +119,13 @@
 				</div>	
 				
 				<div class="mt-3"></div>
-				
-				<script>
-					$(document).ready(function(){
-						$(".form-group > input").keypress(function(event){
-							if( event.type == "keypress" && event.which == 13 )
-								register();
-						});
-					});
-				</script>
 				<?php } ?>
 				
 			</div>			
 			
 		</div>
 		
+	</form>
 	</div>
 
 </div>
